acf8dc26ac1c604e71ad7860528807d282f46dc7
cho <inbrain144@gmail.com>
2024-11-19 05:49:19 +0900

    refact : InvestStatusType 수정
    PENDING, COMPLETED, CANCELLED에서 WATING, EXECUTING, COMPLETED, CANCELED으로 상태 분류 단계를 변경하고, description, detail 을 추가함

--- src/main/java/com/billit/investment/repository/InvestmentRepository.java
+++ src/main/java/com/billit/investment/repository/InvestmentRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface InvestmentRepository extends JpaRepository<Investment, Long> {
+public interface InvestmentRepository extends JpaRepository<Investment, Integer> {
 
     List<Investment> findByUserInvestorId(Long userInvestorId);
 }
