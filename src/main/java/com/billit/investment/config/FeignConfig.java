0069fdbe75b969b221f7c81993db91cb6a459cbc
platinum57 <sook8986@naver.com>
2024-11-28 20:02:14 +0900

    fix: docker 환경에서 api 통신이 되지 않던 현상 수정

--- src/main/java/com/billit/investment/config/FeignConfig.java
+++ src/main/java/com/billit/investment/config/FeignConfig.java
@@ -2,20 +2,29 @@ package com.billit.investment.config;
 
 import feign.Client;
 import feign.RequestInterceptor;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
 @Configuration
 public class FeignConfig {
+    @Value("${feign.client.config.user-service.url}")
+    private String userServiceUrl;
+
+    @PostConstruct
+    public void init() {
+        log.info("Configured user-service URL: {}", userServiceUrl);
+    }
+
     @Bean
     public RequestInterceptor requestInterceptor() {
         return requestTemplate -> {
             requestTemplate.header("X-Service-Name", "investment-service");
-
-            log.info("Final Request Headers: {}", requestTemplate.headers());
-            log.info("Request URL: {}", requestTemplate.url());
+            log.info("Making request to URL: {}", requestTemplate.url());
+            log.info("With headers: {}", requestTemplate.headers());
         };
     }
     @Bean
