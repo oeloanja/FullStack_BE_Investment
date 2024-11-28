0b54c9dca81521e02a0bd702a021930d4bb67437
cho <inbrain144@gmail.com>
2024-11-28 23:37:21 +0900

    fix : 포트폴리오 업데이트 부분 수정
    포트폴리오 생성에 있어 전체 투자금 합계 로직에 오류가 있었는데, 포트폴리오 업데이트 부분도 함께 수정했어야 할것을 누락함

--- src/main/java/com/billit/investment/service/InvestmentPortfolioService.java
+++ src/main/java/com/billit/investment/service/InvestmentPortfolioService.java
@@ -76,15 +76,17 @@ public class InvestmentPortfolioService {
         List<Investment> investments = investmentRepository.findByUserInvestorId(userInvestorId);
 
         BigDecimal totalInvestedAmount = BigDecimal.ZERO;
+        BigDecimal totalPrincipal = BigDecimal.ZERO;
         BigDecimal totalReturnValue = BigDecimal.ZERO;
 
         for (Investment investment : investments) {
             SettlementPrincipalAndProfitGetResponse response = settlementService.getTotalSettlementPrincipalAndProfit(investment.getInvestmentId());
-            totalInvestedAmount = totalInvestedAmount.add(response.getTotalSettlementPrincipal());
+            totalInvestedAmount = totalInvestedAmount.add(investment.getInvestmentAmount());
+            totalPrincipal = totalPrincipal.add(response.getTotalSettlementPrincipal());
             totalReturnValue = totalReturnValue.add(response.getTotalSettlementProfit());
         }
 
-        BigDecimal totalReturnRate = totalReturnValue.divide(totalInvestedAmount, 2, RoundingMode.HALF_UP);
+        BigDecimal totalReturnRate = totalReturnValue.divide(totalPrincipal, 2, RoundingMode.HALF_UP);
 
         portfolio.setTotalInvestedAmount(totalInvestedAmount);
         portfolio.setTotalReturnValue(totalReturnValue);
